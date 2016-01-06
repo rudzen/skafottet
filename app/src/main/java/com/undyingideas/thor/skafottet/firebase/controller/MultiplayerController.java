bccd92eb61cef6378a57b906c520af4433afe308
theis <theis99999@gmail.com>
2016-01-06 14:42:21 +0100

    Firebase MultiplayerController

--- app/src/main/java/com/undyingideas/thor/skafottet/firebase/controller/MultiplayerController.java
+++ app/src/main/java/com/undyingideas/thor/skafottet/firebase/controller/MultiplayerController.java
@@ -1,19 +1,25 @@
 package com.undyingideas.thor.skafottet.firebase.controller;
 
+import android.util.Log;
+
 import com.firebase.client.Firebase;
 import com.undyingideas.thor.skafottet.firebase.DTO.LobbyDTO;
+import com.undyingideas.thor.skafottet.firebase.DTO.PlayerDTO;
+import com.undyingideas.thor.skafottet.highscore.Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by theis on 06-01-2016.
  */
 public class MultiplayerController {
-    ArrayList<LobbyDTO> games;
-    LobbyController lc;
-    PlayerController pc;
+    public ArrayList<LobbyDTO> games;
+    public LobbyController lc;
+    public PlayerController pc;
     final Firebase ref;
     String name;
+    HashMap<String, PlayerDTO> playerList = new HashMap<>();
 
     public MultiplayerController(Firebase ref){
         this.ref = ref;
@@ -30,4 +36,11 @@ public class MultiplayerController {
         return null;
     }
 
+    public void update() {
+        PlayerDTO dto;
+        for(String dtokey : playerList.keySet()) {
+            dto = playerList.get(dtokey);
+            Log.d("firebase update", dto.getName() + "  " + dto.getGameList().size());
+        }
+    }
 }
--- app/src/main/java/com/undyingideas/thor/skafottet/firebase/controller/PlayerController.java
+++ app/src/main/java/com/undyingideas/thor/skafottet/firebase/controller/PlayerController.java
@@ -2,6 +2,7 @@ package com.undyingideas.thor.skafottet.firebase.controller;
 
 import android.util.Log;
 
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
@@ -22,14 +23,10 @@ public class PlayerController {
     final Firebase ref;
     final MultiplayerController mpcRef;
 
-
     public PlayerController(final MultiplayerController mp, final Firebase ref){
         this.ref = ref;
         this.mpcRef = mp;
-    }
-
-    public void login(final String name) {
-
+        ref.child("MultiPlayer").child("Players").addChildEventListener(new NameGetter(mpcRef));
     }
 
     public void createPlayer(final String name) {
@@ -74,3 +71,50 @@ class fireBaseCreate implements Transaction.Handler{
     }
 }
 
+class NameGetter implements ChildEventListener {
+    final MultiplayerController mpcref;
+
+    public NameGetter(MultiplayerController mpcref) {
+        this.mpcref = mpcref;
+    }
+
+    @Override
+    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+        mpcref.playerList.put(dataSnapshot.getKey(), getDTO(dataSnapshot));
+        mpcref.update();
+    }
+
+    @Override
+    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+        Log.d("firebase child changed", dataSnapshot.toString());
+        mpcref.playerList.remove(dataSnapshot.getKey());
+        mpcref.playerList.put(dataSnapshot.getKey(), getDTO(dataSnapshot));
+        mpcref.update();
+    }
+
+    @Override
+    public void onChildRemoved(DataSnapshot dataSnapshot) {
+        mpcref.playerList.remove(dataSnapshot.getKey());
+        mpcref.update();
+    }
+
+    @Override
+    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+        // void
+    }
+
+    @Override
+    public void onCancelled(FirebaseError firebaseError) {
+        // void
+    }
+
+    protected PlayerDTO getDTO(DataSnapshot dataSnapshot) {
+        PlayerDTO dto = new PlayerDTO(dataSnapshot.getKey());
+        dto.setScore(Integer.valueOf(dataSnapshot.child("score").getValue().toString()));
+        if (dataSnapshot.hasChild("gameList"))
+            for(DataSnapshot ds : dataSnapshot.child("gameList").getChildren())
+                dto.getGameList().add(ds.getValue().toString());
+        return dto;
+    }
+}
