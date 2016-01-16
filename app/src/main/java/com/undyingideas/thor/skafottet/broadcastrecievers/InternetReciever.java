8ede29947e2b42a4b3b1424d716117e5c7018d3e
Rudy Kohn <s133235@student.dtu.dk>
2016-01-16 10:19:52 +0100

    Updated InternetReciever structure (copied from Hunterbuddy :)

--- app/src/main/java/com/undyingideas/thor/skafottet/broadcastrecievers/InternetReciever.java
+++ app/src/main/java/com/undyingideas/thor/skafottet/broadcastrecievers/InternetReciever.java
@@ -9,16 +9,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
-import java.util.ArrayDeque;
+import java.util.ArrayList;
 
 /**
  * Stakes a claim for information from the system about the current internet connection state.
- * Observers can ask to be notified about the current state.
+ * Observers can ask to be notified about the current state in the correct order by stacking them up.
+ * Uses {@link InternetRecieverData} to handle the information about the type.
+ * @author rudz
  */
 public class InternetReciever extends BroadcastReceiver {
 
-    private Handler handler = new Handler();
-    private static ArrayDeque<Runnable> observers = new ArrayDeque<>();
+    private final Handler handler = new Handler();
+    private static final ArrayList<InternetRecieverData> observers = new ArrayList<>();
 
     private final static String TAG = "InternetReciever";
 
@@ -26,28 +28,45 @@ public class InternetReciever extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent intent) {
         Log.d(TAG, "Network connectivity change");
         final Bundle bundle = intent.getExtras();
-        CharSequence msg;
         if (bundle != null) {
             final NetworkInfo ni = (NetworkInfo) bundle.get(ConnectivityManager.EXTRA_NETWORK_INFO);
+            final int connectionStatus;
             if (ni != null && ni.getState() == NetworkInfo.State.CONNECTED) {
                 Log.i(TAG, "Network " + ni.getTypeName() + " connected");
-                msg = "Internetforbindelsen oprettet via " + ni.getTypeName();
+                connectionStatus = ni.getType();
             } else if (intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, Boolean.FALSE)) {
-                Log.d(TAG, "There's no network connectivity");
-                msg = "Ingen internet forbindelse.";
+                connectionStatus = -1;
+                Log.d(TAG, "There is no network connectivity");
+            } else {
+                // dirty guard hack
+                connectionStatus = -1;
             }
+            // notify the observers who cares about the current internet state!
+            for (final InternetRecieverData internetRecieverData : observers) {
+                internetRecieverData.setData(connectionStatus);
+                handler.post(internetRecieverData);
+                if (!internetRecieverData.isKeepInReciever()) {
+                    if (removeObserver(internetRecieverData)) {
+                        Log.d(TAG, "Observer removed");
+                    } else {
+                        Log.d(TAG, "Observer kept");
+                    }
+                }
+            }
+            observers.trimToSize();
         }
-        // notify the observers who cares about the current internet state!
-        while (!observers.isEmpty()) {
-            handler.post(observers.pollFirst());
+
+    }
+
+    public static void addObserver(final InternetRecieverData newObserver) {
+        if (!observers.contains(newObserver)) {
+            observers.add(newObserver);
+            Log.d(TAG, "Observer added");
+            Log.d(TAG, "Observers current in stack :" + observers.size());
         }
     }
 
-    public static void addObserver(final Runnable newObserver) {
-        observers.push(newObserver);
-    }
-
-    public static void removeObserver(final Runnable observerToRemove) {
-        observers.remove(observerToRemove);
+    public static boolean removeObserver(final InternetRecieverData observerToRemove) {
+        return observers.remove(observerToRemove);
     }
 }
---
+++ app/src/main/java/com/undyingideas/thor/skafottet/broadcastrecievers/InternetRecieverData.java
@@ -0,0 +1,116 @@
+package com.undyingideas.thor.skafottet.broadcastrecievers;
+
+import android.net.ConnectivityManager;
+import android.os.Build;
+import android.util.Log;
+import android.util.SparseArray;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created on 16-01-2016, 07:28.
+ * Project : skafottet<br>
+ * Generic connection state data class.<br>
+ * Combines observer and DTO for internet connection status.
+ * For use with {@link InternetReciever}.<br>
+ * - Updated for skafottet's data structure.
+ * @author rudz
+ */
+public class InternetRecieverData implements Runnable {
+
+    /* store the destination interface as a WEAK reference, this allows the system to GC the subject
+       if needed. Since it's guarded nothing will happend if the target was destroyed. */
+    private final WeakReference<InternetRecieverInterface> internetRecieverInterfaceWeakReference;
+
+    /* the TAG so we know who we are */
+    private final static String TAG = "InternetRecieverData";
+
+    /* the internet connection state, this is set through the broadcast reciever itself */
+    private int data; // is -1 if no connection!
+
+    /* to let the class add itself back to the InternetReciever stack */
+    private boolean keepInReciever;
+
+    /* simple (and memory efficient) structure to hold the string values of the connection state */
+    public static final SparseArray<String> CONNECTION_INFO = new SparseArray<>(7);
+
+    /* populate the structure with the needed strings */
+    static {
+        CONNECTION_INFO.put(ConnectivityManager.TYPE_MOBILE, "Mobil");
+        CONNECTION_INFO.put(ConnectivityManager.TYPE_WIFI, "Wifi");
+        CONNECTION_INFO.put(ConnectivityManager.TYPE_ETHERNET, "Ethernet");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            CONNECTION_INFO.put(ConnectivityManager.TYPE_VPN, "VPN");
+        }
+        CONNECTION_INFO.put(ConnectivityManager.TYPE_BLUETOOTH, "Bluetooth");
+        CONNECTION_INFO.put(ConnectivityManager.TYPE_DUMMY, "Dummy");
+        CONNECTION_INFO.put(ConnectivityManager.TYPE_MOBILE_DUN, "Mobil DUN");
+    }
+
+    /**
+     * This interface has to be implemented by the target, this ensures that we can send data to it.<br>
+     * This is a combination of controlling the runnable interface + the needs for the {@link InternetReciever}.<br>
+     * In essence, it "replaces" the Runnable interface with a more controlled one.<br>
+     * @author rudz
+     */
+    public interface InternetRecieverInterface {
+        void onInternetStatusChanged(int connectionState);
+        void onInternetStatusChanged(final String connectionState);
+    }
+
+    /**
+     * Required the interface implementation.
+     * @param internetRecieverInterface The object which implements the interface.
+     */
+    public InternetRecieverData(final InternetRecieverInterface internetRecieverInterface) {
+        this(internetRecieverInterface, true);
+    }
+
+    /**
+     * Allows defining manually if this class should be kept by the Internet Reciever.
+     * @param internetRecieverInterface The object which implements the interface.
+     * @param keepInReciever If true, this observer class will not be removed when executed.
+     */
+    public InternetRecieverData(final InternetRecieverInterface internetRecieverInterface, final boolean keepInReciever) {
+        internetRecieverInterfaceWeakReference = new WeakReference<>(internetRecieverInterface);
+        this.keepInReciever = keepInReciever;
+    }
+
+    /**
+     * The runnable interface is started here, this will make sure there still is a reference to the master class.
+     * If the references is obtained, it will send the data back to it through its own interface.
+     */
+    @Override
+    public void run() {
+        Log.d(TAG, "Observer runnable started.");
+        final InternetRecieverInterface internetRecieverInterface = internetRecieverInterfaceWeakReference.get();
+        if (internetRecieverInterface != null) {
+            internetRecieverInterface.onInternetStatusChanged(data);
+            if (data > -1) {
+                internetRecieverInterface.onInternetStatusChanged(CONNECTION_INFO.get(data));
+            } else {
+                internetRecieverInterface.onInternetStatusChanged("Ingen");
+            }
+        }
+    }
+
+    /**
+     * Setter for connection state, this is invokes through the reciever.
+     * @param newData The connection state of the system
+     */
+    public void setData(final int newData) {
+        data = newData;
+    }
+
+    /**
+     * Sets the keepInReciever feature
+     * @param newValue If true, it will not be removed from the internet reciever.
+     */
+    public void setKeepInReciever(final boolean newValue) {
+        keepInReciever = newValue;
+    }
+
+    public boolean isKeepInReciever() {
+        return keepInReciever;
+    }
+}
