c78640fcc634a4fa71cf84d762e181d89ba56336
Rudy Kohn <s133235@student.dtu.dk>
2016-01-16 21:54:35 +0100

    Updated about fragment

--- app/src/main/java/com/undyingideas/thor/skafottet/fragments/AboutFragment.java
+++ app/src/main/java/com/undyingideas/thor/skafottet/fragments/AboutFragment.java
@@ -40,16 +40,30 @@ public class AboutFragment extends Fragment {
             "Skafottet - et 3-ugers projekt, af Gruppe 23" + SEP +
                     "Gruppe medlemmer der har tilføjet til projektet :" + SEP +
                     "s133235 Rudy Alex Kohn, s133235@student.dtu.dk" + SEP +
-                    "sXXXXXX Adam Hammer Palmar, sXXXXXX@student.dtu.dk" + SEP +
-                    "sXXXXXX Theis Friis Strømming, sXXXXXX@student@dtu.dk" + SEP +
+                    "s144868 Adam Hammer Palmar, s144868@student.dtu.dk" + SEP +
+                    "s130063 Theis Friis Strømming, s130063@student.dtu.dk" + SEP +
             "Projekt ansvarlig : Rudy Alex Kohn" +
                     SEP + SEP +
-                    "Design / Grafik / Kode : " + RUDY + SEP +
-                    "Firebase Design / Kode : " + ADAM + ", " + THEIS + SEP + SEP + " " + SEP +
-                    "Baseret på originalt skafott af Thor" + SEP + SEP +
+                    "Overordnet Design / Grafik / Kode : " + RUDY + SEP +
+                    "Menu Design / Kode : " + RUDY + SEP +
+                    "Galge 3D Kode : " + THEIS + SEP +
+                    "Firebase Design / Kode : " + THEIS + SEP +
+                    "Yderligere Firebase Design / Kode : " + ADAM + SEP + SEP + " " + SEP +
+                    "Baseret på originalt skafott design af Thor" + SEP + SEP + SEP + SEP +
+                    "Dette projekt indeholder følgende : " + SEP +
+                    "Delvist fragment baseret design med parcel dataoverførsel, Ny Galgelogik, Savegame, Grafik (Herunder sensorstyret flertrådet stjernebaggrund samt 3D galge med touchevents), Animationer, Vibrator, Sensor (Tyngdekraft -> Accelerometer) (Benyttet til menu stjernebaggrund), " +
+                    "TextView med understreget tekst, AsyncTasks, Specielt designede Broadcast Recievers struktur med overtagelse af Runnable Interface (Internet adgang, batteri status),  Tråde (Via custom Observere til broadcast recievers), " +
+                    "Parcels, Serialiseret filgemning via GZIPstream (Ordlister), Ordliste tilføjelse (fra internet addresser), Ord-filtrering, Ordliste opdatering, Ordliste sletning, " +
+                    "Firebase forbindelse, Firebase opdateringer af informationer, Firebase Multiplayer funktionalitet (inkl. simpel login), Firebase highscore, Hjælpe funktion via asset html, " +
+                    "Programatisk opsætning af skærmen, Baggrundsmusik via forgrundsservice, Lydeffekter, Indstillingsmuligheder, Adaptere (Array, RecycleView, BaseAdapter) inkl. Viewholders." + SEP + SEP +
 
             "Original galgelogik af Jakob Nordfalk." + SEP + "http://www.javabog.dk",
-            "TinyDB preferences interface af kcochibili." + SEP + "https://github.com/kcochibili/TinyDB--Android-Shared-Preferences-Turbo",
+            "TinyDB preferences interface af kcochibili." + SEP + "https://github.com/kcochibili/TinyDB--Android-Shared-Preferences-Turbo" + SEP + SEP
+            + SEP + SEP + "Ingen dyr led nød under udviklingen af denne app, ej heller under brugen af samme. Vi forventer invasion fra en flok tapirer i nær fremtid, så det skal slås fast vi er dyrevenner!"
+
+
+
+            ,
             "\"THE BEER-WARE LICENSE\" (Revision 42):" + SEP +
                     "  ~ <s133235@student.dtu> wrote this file. As long as you retain this notice you" + SEP +
                     "  ~ can do whatever you want with this stuff. If we meet some day, and you think" + SEP +
